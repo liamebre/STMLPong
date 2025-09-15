f735ec27d99dc03a480af771ff39961918796a9b
liamebre <164887882+liamebre@users.noreply.github.com>
2025-09-15 14:58:39 -0400

    Create include.h
    stores all inclusions minus redundances

---
+++ include.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <SFML/Audio.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "player.h"	
+
+using namespace sf;
+using namespace std;
